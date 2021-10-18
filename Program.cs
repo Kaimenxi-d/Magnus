828c7448196a8c8369f2bd77a8a45b154cf77fa1
Said-Emin <emuertepin@gmail.com>
2021-10-18 15:23:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Mains
+    {
+        static void Main(string[] args)
+        {
+            Doch1 sm = new Doch1();
+            Doch1.PrintUsername();
+            Doch2 ms = new Doch2();
+            Doch2.Ds();
+        }
+    }
+    class Employee
+    {
+        public static void PrintUsername()
+        {
+            Console.Write("Введите своё имя:");
+            string ft = Console.ReadLine();
+            Console.WriteLine($"Имя пользователя:{ft}");
+            Console.ReadKey();
+        }
+        public static void Ds()
+        {
+            Console.WriteLine("Лаба Ильяса");
+        }
+
+    }
+    class Doch1:Employee
+    {
+
+
+    }
+    class Doch2:Employee
+    {
+
+
+    }
+    class Doch3
+    {
+
+
+    }
+}
